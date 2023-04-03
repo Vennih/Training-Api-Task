fd7d75cc788eefcaf7bf5f869ad616aaa26ba609
Vennih <keasav1436@gmail.com>
2023-04-03 13:04:10 +0530

    name concatenation update

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
@@ -9,37 +8,44 @@ import { EmailValidator } from '@angular/forms';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
-  userData: any;
-  fullName: any;
 
-  fullNameFromFirstName: any = "";
-  fullNameFromLastName: any = "";
-  firstNameInput: any;
-  lastNameInput: any;
-  regex:any = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+  firstnameInput: string = "";
+  lastnameInput: string = "";
+  regex: any = /[^\s@]+@[^\s@]+\.[^\s@]+/;
 
   constructor(private http: HttpClient, private route: Router) { }
 
   ngOnInit(): void {
 
-    this.fullName = document.querySelector("#inputFullName4");
-
   }
 
+  get fullname(): string {
+    const name = this.model.firstname + ' ' + this.model.lastname;
+    return name
+  }
+
+  model = {
+    firstname: '',
+    lastname: '',
+    name: ''
+  }
+
+
   AddUser(data: any) {
-    console.log(data)
+    console.log(data);
+    data.name = this.model.firstname + " " + this.model.lastname;
     if (data.name != "" && data.email != "" && data.gender != "" && data.status != "") {
       if (data.gender == 'male' || data.gender == 'female') {
         if (data.status == 'active' || data.status == 'inactive') {
-          if(this.regex.test(data.email)){
-          this.http.post("https://gorest.co.in/public/v2/users?access-token=7b319b308eb19b622798bbd47e959e1b301a43e48f3e6ccdad84a9746ba35525", data)
-            .subscribe(res => {
-              console.log(res)
-              alert("User Details Added Successfully")
-              this.route.navigate(['userlist'])
-            })
-          console.log(data)
-          }else { alert("Enter a valid email") }
+          if (this.regex.test(data.email)) {
+            this.http.post("https://gorest.co.in/public/v2/users?access-token=7b319b308eb19b622798bbd47e959e1b301a43e48f3e6ccdad84a9746ba35525", data)
+              .subscribe(res => {
+                console.log(res)
+                alert("User Details Added Successfully")
+                this.route.navigate(['userlist'])
+              })
+            console.log(data)
+          } else { alert("Enter a valid email") }
         } else { alert("Enter active or inactive only in Status") }
       } else { alert("Enter male or female only in Gender") }
     } else { alert("Please fill all the user details") }
@@ -47,22 +53,6 @@ export class AddUserComponent implements OnInit {
 
   goToUserList() {
     this.route.navigate(["userlist"]);
-  }
-
-  showFirstNameInFullName(event: Event) {
-    const filvalue = (event.target as HTMLInputElement).value;
-    this.fullNameFromFirstName = filvalue
-    this.fullNameValue()
-  }
-  showLastNameInFullName(event: Event) {
-    const filvalue = (event.target as HTMLInputElement).value;
-    this.fullNameFromLastName = filvalue
-    this.fullNameValue()
-  }
-
-  fullNameValue() {
-    this.fullName.value = this.fullNameFromFirstName + " " + this.fullNameFromLastName;
-    console.log(this.fullName.value)
   }
 
 }
