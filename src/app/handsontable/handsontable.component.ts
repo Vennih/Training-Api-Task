1cf2a27e652d394fb8a1bdad977a094d453bda18
Vennih <123716587+Vennih@users.noreply.github.com>
2023-04-12 13:06:53 +0530

    12-4-2023 initial commit

--- src/app/handsontable/handsontable.component.ts
+++ src/app/handsontable/handsontable.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import Handsontable from 'handsontable';
 import { HttpClient } from '@angular/common/http';
+import { data } from 'jquery';
 
 @Component({
   selector: 'app-handsontable',
@@ -9,57 +10,65 @@ import { HttpClient } from '@angular/common/http';
 })
 
 
-export class HandsontableComponent implements OnInit {
-
-  tableData: any = [];
+export class HandsontableComponent implements OnInit, AfterViewInit {
 
   @ViewChild("hotTable")
   hotTable!: ElementRef;
+
+  tableData: any = [];
+  objecttoarray: any;
 
 
 
   constructor(private http: HttpClient) {
 
   }
-  ngOnInit(): void {
+  ngOnInit() {
 
     this.http.get('https://gorest.co.in/public/v2/users?access-token=7b319b308eb19b622798bbd47e959e1b301a43e48f3e6ccdad84a9746ba35525')
       .subscribe(data => {
         this.tableData = data;
+        this.objecttoarray = this.tableData.map((x: { id: number; }) => Object.values(x))
+
+        console.log(this.objecttoarray)
 
         this.ngAfterViewInit()
       });
-    // console.log(this.tableData);
 
   }
 
 
   ngAfterViewInit() {
-    // console.log(typeof this.hotTable);
-    console.log(this.tableData);
 
     const hotElement = this.hotTable.nativeElement;
     const hotSettings = {
       licenseKey: "non-commercial-and-evaluation",
-      data: this.tableData,
-      // colHeaders: ['Id', 'Title', 'Brand', 'Category', 'Price','rating', 'Stock'],      
+      data: this.objecttoarray,
       colHeaders: ['Id', 'Name', 'Email', 'Gender', 'Status'],
-      // columnHeader: true,
       columns: [
-        { type: 'numeric' },
-        { type: 'text' },
-        { type: 'text' },
-        {
-          type: dropdownMenu: [
-            'remove_col',
-            '---------',
-            'make_read_only',
-            '---------',
-            'alignment'
-          ] },
-        // { type: 'date', dateFormat: 'M/D/YYYY' },
-        { type: 'text' }
+        { type: 'numeric', data: 0 },
+        { type: 'text', data: 1 },
+        { type: 'text', data: 2 },
+        { type: 'dropdown', source: ['Male', 'Female'], allowInvalid: false, data: 3 },
+        { type: 'dropdown', source: ['active', 'inactive'], allowInvalid: false, data: 4 }
       ],
+      // columns: function(column: number) {
+      //   let columnMeta = {};
+    
+      //   if (column === 0) {
+      //     columnMeta.tableData = 'id';
+      //   } else if (column === 1) {
+      //     columnMeta.tableData = 'name';
+      //   } else if (column === 2) {
+      //     columnMeta.tableData = 'email';
+      //   } else if (column === 3) {
+      //     columnMeta.tableData = 'gender';
+      //   } else {
+      //     columnMeta = null;
+      //   }
+    
+      //   return columnMeta;
+      // },
       rowHeaders: true,
       width: '100%',
       height: 400,
@@ -71,6 +80,7 @@ export class HandsontableComponent implements OnInit {
       manualRowMove: true,
       manualColumnMove: true,
       bindRowsWithHeaders: true,
+
     };
     const hot = new Handsontable(hotElement, hotSettings);
   }
